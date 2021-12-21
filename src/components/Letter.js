55beb551a2c1318cbeffc0b2a606c98095fcac17
majdjb7 <majdjd171@gmail.com>
2021-12-21 23:07:01 +0200

    passed letters as props

--- src/components/Letter.js
+++ src/components/Letter.js
@@ -1,17 +1,9 @@
 import React, { Component } from 'react';
 
 class Letter extends Component {
-    render(props) {
-        let alphabets = []
-        for(let i=0; i<26; i++) {
-            let letter = (i+10).toString(36).toUpperCase()
-            alphabets.push(letter)
-        }
-        // let letter = (i+10).toString(36).toUpperCase()
+    render() {
         return (
-            <div>
-            {alphabets.map(a => <span>{a}</span>)}
-            </div>
+            <span>{this.props.letter}</span>
         )
     }
 
--- src/components/Letters.js
+++ src/components/Letters.js
@@ -2,11 +2,22 @@ import React, { Component } from 'react';
 import Letter from './Letter';
 
 class Letters extends Component {
+    constructor() {
+        super()
+        this.state = {
+            alphabets: []
+        }
+    }
     render() {
+        for(let i=0; i<26; i++) {
+            let letter = (i+10).toString(36).toUpperCase()
+            this.state.alphabets.push(letter)
+        }
         return (
             <div>
                 <div>Available Letters</div>
-                <Letter/>
+                {this.state.alphabets.map(a => <Letter letter={a}/>)}
+                {/* <Letter alphabets={this.state.alphabets}/> */}
             </div>
         )
     }
